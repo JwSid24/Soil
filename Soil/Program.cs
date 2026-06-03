5ee827ff79cfcbf9df3ad8ec248636a2d45e7aeb
JwSid24 <jwan.sidoo9@gmail.com>
2026-06-03 22:05:00 +0200

    lidt mere refactor

--- Soil/Program.cs
+++ Soil/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -22,11 +22,11 @@ builder.Services.AddScoped<ISoilService, SoilService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
